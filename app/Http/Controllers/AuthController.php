a432a139fa3760e33521d542f4679f44c73e3a5f
pr <prankst3r.mail@gmail.com>
2025-05-27 22:20:04 +0500

    add login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -14,7 +15,7 @@ class AuthController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed'
+            'password' => 'required|string|min:8'
         ]);
 
         User::create([
@@ -24,5 +25,21 @@ class AuthController extends Controller
         ]);
 
         return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $request->user()->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token], 200);
     }
 }
